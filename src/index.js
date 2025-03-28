e2808fcc4581dbf9ff73500dd3d7d07d489f04cb
dallocdalloc <dwpark@donga.ac.kr>
2025-03-28 11:26:18 +0900

    chapter3 completion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import firebase from './firebase';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(firebase);
+
+const root = createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root"),
 );
