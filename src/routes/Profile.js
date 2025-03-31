9ac4046a08a3db595f638a08627035ed8efd4458
dallocdalloc <dwpark@donga.ac.kr>
2025-03-31 10:43:34 +0900

    chapter4 completion

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,20 @@
-const Profile = () => <span>Profile</span>
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+    const navigate = useNavigate();
+
+    const onLogOutClick = () => {
+        authService.signOut();
+        // history.push("/");
+        navigate("/");
+    };
+
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
 
 export default Profile;
