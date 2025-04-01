6c50cf0e10e63de5af413360197de293da226cd7
dallocdalloc <dwpark@donga.ac.kr>
2025-04-01 14:26:27 +0900

    chapter6 completion but no test save image

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { useEffect, useState } from "react";
 import Nweet from "components/Nweet";
+import { v4 as uuidv4 } from "uuid";
 
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
@@ -21,13 +22,19 @@ const Home = ({userObj}) => {
     const onSubmit = async (event) => {
         event.preventDefault();
 
+        const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+        const response = await attachmentRef.putString(attachment, "data_url");
+        const attachmentUrl = await response.ref.getDownloadURL();
+
         await dbService.collection("nweets").add({
             text: nweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
+            attachmentUrl,
         });
 
         setNweet("");
+        setAttachment("");
     };
     
     const onChange = (event) => {
